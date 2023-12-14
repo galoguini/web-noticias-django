73cf30ec613b9929761ad4612dee2656c4776d8d
galoguini <galtahgui.ni@gmail.com>
2023-12-13 21:12:32 -0300

    Agregado de mas herencia y tablas
    implementación de herencia en el header, agregado de la tabla categoría para las noticias (con migraciones ya hechas), y un botón para usuarios admin en los perfiles

--- apps/noticias/models.py
+++ apps/noticias/models.py
@@ -2,12 +2,24 @@ from django.db import models
 
 # Create your models here.
 
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = 'categoria'
+        verbose_name_plural = 'categorias'
+        ordering = ['nombre']
+
+    def __str__(self):
+        return self.nombre
+
 class Noticia(models.Model):
     titulo = models.CharField(max_length=100)
     contenido = models.TextField()
     imagen = models.ImageField(upload_to='noticias')
     creado = models.DateTimeField(auto_now_add=True)
     modificado = models.DateTimeField(auto_now=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT)
 
     class Meta:
         verbose_name = 'noticia'
