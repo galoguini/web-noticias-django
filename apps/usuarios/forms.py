82c1f85c9a8b025526dc1cde0ec92ddc596623b4
galoguini <galtahgui.ni@gmail.com>
2023-12-17 18:15:03 -0300

    modificaciones al registro
    agregado de validaciones a la hora de registrarse y cambios menores en cuanto al frontend

--- apps/usuarios/forms.py
+++ apps/usuarios/forms.py
@@ -2,8 +2,16 @@ from django import forms
 
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
 
 class RegistroForm(UserCreationForm):
+    email = forms.EmailField(
+        error_messages={
+            'required': '⚠️Este campo es requerido.',
+            'invalid': '⚠️Introduzca una dirección de email válida.',
+        }
+    )
 
     class Meta:
         model = User
@@ -15,3 +23,22 @@ class RegistroForm(UserCreationForm):
             'password1',
             'password2'
         ]
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(username=username).exists():
+            raise ValidationError('⚠️El nombre de usuario ya existe.')
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')    
+        if User.objects.filter(email=email).exists():
+            raise ValidationError('⚠️Ya existe un usuario con ese correo electrónico.')
+        return email
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError('⚠️Las contraseñas no coinciden.')
+        return password2
--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -1,17 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from .forms import RegistroForm
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
 
 # Create your views here.
 def login(request):
     return render(request, 'usuarios/login.html')
 
 class registro(CreateView):
-	#FORMULARIO DJANGO
-	form_class = RegistroForm
-	success_url = reverse_lazy('usuarios:login')
-	template_name = 'usuarios/registro.html'
+    form_class = RegistroForm
+    success_url = reverse_lazy('usuarios:login')
+    template_name = 'usuarios/registro.html'
 
 def perfil(request):
 	return render(request, 'usuarios/perfil.html')
