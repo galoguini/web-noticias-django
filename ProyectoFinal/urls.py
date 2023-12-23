73379fcbd7b3fbbf918cd1d508410934b84033fe
galoguini <galtahgui.ni@gmail.com>
2023-12-23 19:10:24 -0300

    CAMBIOS MAYORES
    -implementación de una nueva plantilla, con bootstrap para los estilos y fontawesome para los iconos (también jquery y js)
    -agregado de "titulo" a la herencia del html base
    -agregado de temperatura y clima de la ciudad de Resistencia (mediante la api de "weatherapi.com")
    -agregada la posibilidad de restablecer contraseña mediante un email al email del usuario que lo solicita. con sus respectivos urls, vistas y htmls
    -purga/optimización de código en algunos archivos

--- ProyectoFinal/urls.py
+++ ProyectoFinal/urls.py
@@ -20,10 +20,7 @@ from . import views
 from django.conf import settings 
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
-from django.conf import settings 
-from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.auth import views as auth
 
 urlpatterns = [
     path('admin/', admin.site.urls),
