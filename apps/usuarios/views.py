6bd28c4528d67809aa9b0b531152634b137b74fd
galoguini <galtahgui.ni@gmail.com>
2023-12-27 02:27:43 -0300

    Modif. de Perfil y mas.
    -agregado de perfiles personales (antes solo se podía ver el perfil del usuario actualmente logueado)
    -modificación de la url y la vista de perfil, para que cualquier persona, logueda o no, pueda ver los perfiles. no solo el usuario logueado
    -agregado de una sección "noticias publicadas" en el perfil, el cual muestra  todas las noticias publicadas por ese usuario.
    -agregado de términos y condiciones a la hora de registrarse (con su css).
    -agregado de una noticia destacada en la parte superior de la pagina web (mediante un context processor en la app de noticias)
    -agregado de favicon
    -purga/optimización de código

--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -4,9 +4,14 @@ from django.urls import reverse_lazy
 from .forms import RegistroForm
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.http import Http404
+from apps.noticias.models import Noticia
 from django.contrib.auth.decorators import login_required
 from .forms import FotoPerfilForm, EditarNombreForm, EditarEmailForm, EditarUsuarioForm, EditarContraseñaForm
 from django.contrib.auth.views import PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+
+def terminos_condiciones(request):
+    return render(request, 'usuarios/term_cond.html')
 
 class ResetContraseñaView(PasswordResetView):
     template_name = 'usuarios/reset_contraseña.html'
@@ -31,9 +36,17 @@ class registro(CreateView):
     success_url = reverse_lazy('usuarios:login')
     template_name = 'usuarios/registro.html'
 
-@login_required
-def perfil(request):
-    return render(request, 'usuarios/perfil.html')
+def perfil(request, username):
+    try:
+        user = User.objects.get(username=username)
+        noticias = Noticia.objects.filter(usuario=user)
+    except User.DoesNotExist:
+        raise Http404("Usuario no existe")
+    
+    if request.user.username == username:
+        return render(request, 'usuarios/perfil.html', {'user': user, 'noticias': noticias})
+    elif request.user.username != username:
+        return render(request, 'usuarios/perfil.html', {'user': user, 'noticias': noticias})
 
 @login_required
 def update_foto_perfil(request):
@@ -41,7 +54,7 @@ def update_foto_perfil(request):
         form = FotoPerfilForm(request.POST, request.FILES, instance=request.user.userprofile)
         if form.is_valid():
             form.save()
-            return redirect('usuarios:perfil')
+            return redirect('usuarios:perfil', request.user.username)
         else:
             print(form.errors)
     else:
@@ -55,7 +68,7 @@ def editar_nombre(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Tu perfil ha sido actualizado.')
-            return redirect('usuarios:perfil')
+            return redirect('usuarios:perfil', request.user.username)
     else:
         form = EditarNombreForm(instance=request.user)
     return render(request, 'usuarios/editar_nombre.html', {'form': form})
@@ -67,7 +80,7 @@ def editar_email(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Tu email ha sido actualizado.')
-            return redirect('usuarios:perfil')
+            return redirect('usuarios:perfil', request.user.username)
     else:
         form = EditarEmailForm(instance=request.user)
     return render(request, 'usuarios/editar_email.html', {'form': form})
@@ -79,7 +92,7 @@ def editar_usuario(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Tu nombre de usuario ha sido actualizado.')
-            return redirect('usuarios:perfil')
+            return redirect('usuarios:perfil', request.user.username)
     else:
         form = EditarUsuarioForm(instance=request.user)
     return render(request, 'usuarios/editar_usuario.html', {'form': form})
@@ -91,7 +104,7 @@ def editar_contraseña(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Tu contraseña ha sido actualizada.')
-            return redirect('usuarios:perfil')
+            return redirect('usuarios:perfil', request.user.username)
     else:
         form = EditarContraseñaForm(instance=request.user)
     return render(request, 'usuarios/editar_contraseña.html', {'form': form})
