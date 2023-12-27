e555f0144e9a2bef61af387e5f7a1d1f41d41d67
galoguini <galtahgui.ni@gmail.com>
2023-12-27 17:21:47 -0300

    cambios menores
    pequeña edición del urls principal

--- ProyectoFinal/urls.py
+++ ProyectoFinal/urls.py
@@ -20,8 +20,6 @@ from . import views
 from django.conf import settings 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns 
 from django.conf.urls.static import static
-from django.contrib.auth import views as auth
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -32,4 +30,4 @@ urlpatterns = [
     path('contacto/', include('apps.contactos.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += staticfiles_urlpatterns()
-urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
