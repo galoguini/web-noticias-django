daf5ae4077d5f24604aef93f9b0dc2e4321bf007
matiasszalazar <biancam.dev@gmail.com>
2023-12-28 01:40:28 -0300

    Creación del Home principal
    - Creación del archivo home.css
    - Edición de archivo views.py
    - Edición de archivo urls.py
    - Edición de archivo home.html, bucles for para mostrar Últimas Noticias por Categorías y condicionante if para que sólo se muestren las noticias de cada categoría en su respectiva tarjeta.

--- ProyectoFinal/urls.py
+++ ProyectoFinal/urls.py
@@ -20,11 +20,11 @@ from . import views
 from django.conf import settings 
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.contrib.auth import views as auth
+from .views import HomeView
 
 urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
     path('noticias/', include('apps.noticias.urls')),
     path('usuarios/', include('apps.usuarios.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- ProyectoFinal/views.py
+++ ProyectoFinal/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from apps.noticias.models import Noticia, Categoria
 
-def home(request):
-    return render(request, 'home.html')
+class HomeView(ListView):
+    model = Noticia
+    template_name = 'home.html'
+    context_object_name = 'home_principal_n'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        categorias = Categoria.objects.all()
+        noticias_por_categoria = {categoria: Noticia.objects.filter(categoria=categoria)[:2] for categoria in categorias}
+        context['noticias_por_categoria'] = noticias_por_categoria
+        return context
